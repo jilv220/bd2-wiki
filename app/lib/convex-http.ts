2651344732e72a5c87dd18e4214393bc75e34914
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-01-27 22:38:51 -0600

    huh?

--- app/lib/convex-http.ts
+++ app/lib/convex-http.ts
@@ -1,3 +1,6 @@
 import { ConvexHttpClient } from "convex/browser";
 
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 export const convexHttp = new ConvexHttpClient(process.env.VITE_CONVEX_URL!);
