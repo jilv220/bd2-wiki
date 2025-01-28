63a474403c25f63b557fb9d52b998271f15912eb
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-01-27 23:07:52 -0600

    still not sure?

--- app/lib/convex-http.ts
+++ app/lib/convex-http.ts
@@ -1,6 +1,6 @@
 import { ConvexHttpClient } from "convex/browser";
 
 // Don't access env in global for cloudflare...
-export const getConvexHttp = () => {
-	return new ConvexHttpClient(process.env.VITE_CONVEX_URL!);
+export const getConvexHttp = (env: string) => {
+	return new ConvexHttpClient(env);
 };
